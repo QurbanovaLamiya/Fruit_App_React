b2440a168ff17397968e8370036bf6a8934119c2
QurbanovaLamiya <lamiyaqurbanova121@gmail.com>
2022-10-15 10:10:09 +0400

    Example

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component, Fragment } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <Fragment></Fragment>;
+  }
+}
+
+export default App;
